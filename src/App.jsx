372b5ba65fc1aeafb2e40c23ab5e970d5f7cde9e
boogeyman <abannepal21@gmail.com>
2024-08-29 20:07:00 +0545

    recommit;ViewRepo new component; corrected the link;

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Note from "./components/Notice"
 import SearchFilter from "./components/SearchFilter"
+import ViewRepo from "./components/ui/ViewRepo"
 
 function App() {
 
   return (
     <>
+      <ViewRepo/>
       <Note />
       <SearchFilter className="bg-violet-1000" />
     </>
---
+++ src/components/ui/ViewRepo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ViewRepo = () => {
+  return (
+    <div className="max-sm:text-sm shadow-lg py-2 text-center bg-red-100 border-b-2 border-highlight-color/45">
+      <h2 className="font-medium underline underline-offset-4 text-md italic">
+        <a
+          href="https://github.com/anubhavnepal/task-1-search-functionality"
+          target="_blank"
+        >
+          Repo Link{" "}
+        </a>
+
+      </h2>
+    </div>
+  );
+};
+
+export default ViewRepo;
